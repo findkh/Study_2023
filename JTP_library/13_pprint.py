ff8b36147c2e0b3571307c9ba30331a528db8b9e
findkh <findkh@nate.com>
2023-01-29 23:11:35 +0900

    라이브러리 - pprint, bisect

---
+++ JTP_library/13_pprint.py
@@ -0,0 +1,12 @@
+import pprint
+
+# 13. 데이터를 보기 좋게 출력
+
+result = {
+    'userId': 1, 
+    'id': 1, 
+    'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', 
+    'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+}
+print(result)
+pprint.pprint(result)
