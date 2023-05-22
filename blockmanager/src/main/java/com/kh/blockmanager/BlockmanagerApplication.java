934ae9591dcd4d836893ecf9ad7dfb37c5c2d19c
findk <findkh@nate.com>
2023-05-22 21:16:05 +0900

    2023.05.22. Block Manager 계획 수립

---
+++ blockmanager/src/main/java/com/kh/blockmanager/BlockmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.kh.blockmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockmanagerApplication.class, args);
+	}
+
+}
