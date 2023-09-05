62bcb818c1f7a1a79dd3e370a9eb0be1d3fdd90a
findk <findkh@nate.com>
2023-09-05 20:19:44 +0900

    23.09.05 컴포넌트

---
+++ react_study/component-react/src/App.js
@@ -0,0 +1,22 @@
+import Say from './Say';
+
+const App = () => {
+  return <Say />;
+};
+
+// import Counter from "./Counter";
+
+// const App = () => {
+//   return <Counter />;
+// };
+
+// import MyComponet from "./MyComponent";
+
+// const App = () => {
+//   return (
+//     <MyComponet name={"React"} favoriteNumber={2}>
+//       리액트
+//     </MyComponet>);
+// }
+
+export default App;
