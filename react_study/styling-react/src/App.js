3756ee205418ca864ac320a9565d4ad8bce4b980
findk <findkh@nate.com>
2023-09-22 20:01:50 +0900

    23.09.22. 컴포넌트 스타일링

--- react_study/styling-react/src/App.js
+++ react_study/styling-react/src/App.js
@@ -1,17 +1,47 @@
-import React, { Component } from 'react';
-import SassComponent from './SassComponent';
- 
+import { Component } from 'react'
+import StyledComponent from './StyledComponent'
+
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <SassComponent />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <StyledComponent />
+            </div>
+        );
+    }
 }
- 
+
 export default App;
+
+// import { Component } from 'react'
+// import CSSModule from './CSSModule'
+
+// class App extends Component {
+//     render(){
+//         return(
+//             <div>
+//                 <CSSModule/>
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
+
+// import React, { Component } from 'react';
+// import SassComponent from './SassComponent';
+ 
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <SassComponent />
+//       </div>
+//     );
+//   }
+// }
+ 
+// export default App;
 
 // import { Component } from 'react';
 // import logo from './logo.svg';
