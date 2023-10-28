521eef4381a12ce061f935f826b210d6e2614ba4
findk <findkh@nate.com>
2023-10-28 10:47:47 +0900

    23.10.28. 외부 API 연동 뉴스뷰어만들기

--- react_study/news-viewer/src/App.js
+++ react_study/news-viewer/src/App.js
@@ -1,57 +1,13 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { Route, Routes } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-	const [data, setData] = useState(null);
-	const onClick = async () => {
-		try {
-			const response = await axios.get(
-				'https://jsonplaceholder.typicode.com/todos/1',
-			);
-			setData(response.data);
-		} catch (e) {
-			console.log(e);
-		}
-	};
 	return (
-		<div>
-			<div>
-				<button onClick={onClick}>불러오기</button>
-			</div>
-			{data && (
-				<textarea
-					rows={7}
-					value={JSON.stringify(data, null, 2)}
-					readOnly={true}
-				/>
-			)}
-		</div>
+		<Routes>
+			<Route path="/" element={<NewsPage />} />
+			<Route path="/:category" element={<NewsPage />} />
+		</Routes>
 	);
 };
-
-// const App = () => {
-// 	const [data, setData] = useState(null);
-// 	const onClick = () => {
-// 		axios
-// 			.get('https://jsonplaceholder.typicode.com/todos/1')
-// 			.then((response) => {
-// 				setData(response.data);
-// 			});
-// 	};
-// 	return (
-// 		<div>
-// 			<div>
-// 				<button onClick={onClick}>불러오기</button>
-// 			</div>
-// 			{data && (
-// 				<textarea
-// 					rows={7}
-// 					value={JSON.stringify(data, null, 2)}
-// 					readOnly={true}
-// 				/>
-// 			)}
-// 		</div>
-// 	);
-// };
 
 export default App;
